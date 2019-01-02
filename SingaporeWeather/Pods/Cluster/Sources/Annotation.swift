46603828e321f9aad4e5bcf96e97dd08bed21a0e
Leslie Zhang <sschipmunks@gmail.com>
2019-01-02 23:31:21 +0800

    导入地图控件

---
+++ SingaporeWeather/Pods/Cluster/Sources/Annotation.swift
@@ -0,0 +1,68 @@
+//
+//  Annotation.swift
+//  Cluster
+//
+//  Created by Lasha Efremidze on 4/15/17.
+//  Copyright © 2017 efremidze. All rights reserved.
+//
+
+import MapKit
+
+open class Annotation: MKPointAnnotation {
+    public convenience init(coordinate: CLLocationCoordinate2D) {
+        self.init()
+        self.coordinate = coordinate
+    }
+}
+
+open class ClusterAnnotation: Annotation {
+    open var annotations = [MKAnnotation]()
+    
+    open override func isEqual(_ object: Any?) -> Bool {
+        guard let object = object as? ClusterAnnotation else { return false }
+        
+        if self === object {
+            return true
+        }
+        
+        if coordinate != object.coordinate {
+            return false
+        }
+        
+        if annotations.count != object.annotations.count {
+            return false
+        }
+        
+        return annotations.map { $0.coordinate } == object.annotations.map { $0.coordinate }
+    }
+}
+
+open class ClusterAnnotationView: MKAnnotationView {
+    
+    open lazy var countLabel: UILabel = {
+        let label = UILabel()
+        label.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        label.backgroundColor = .clear
+        label.font = .boldSystemFont(ofSize: 13)
+        label.textColor = .white
+        label.textAlignment = .center
+        label.adjustsFontSizeToFitWidth = true
+        label.minimumScaleFactor = 0.5
+        label.baselineAdjustment = .alignCenters
+        self.addSubview(label)
+        return label
+    }()
+    
+    open override var annotation: MKAnnotation? {
+        didSet {
+            configure()
+        }
+    }
+    
+    open func configure() {
+        guard let annotation = annotation as? ClusterAnnotation else { return }
+        let count = annotation.annotations.count
+        countLabel.text = "\(count)"
+    }
+    
+}
