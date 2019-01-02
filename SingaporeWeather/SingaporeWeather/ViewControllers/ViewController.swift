b330591accf742e4b7e4dca59b0fd4ee36fa1b90
Leslie Zhang <sschipmunks@gmail.com>
2019-01-02 22:59:12 +0800

    项目初始化

---
+++ SingaporeWeather/SingaporeWeather/ViewControllers/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  SingaporeWeather
+//
+//  Created by Leslie Zhang on 2019/1/2.
+//  Copyright © 2019 Leslie Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = UIColor.white
+    }
+
+
+}
+
